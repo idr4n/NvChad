6438a0191cb2dd2f24c0710b4a14c13a5f3558fa
Ivan Duran <idurandiaz@gmail.com>
2024-09-01 22:38:45 +0300

    Add img-clip plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1000,4 +1000,21 @@ return {
       "nvim-telescope/telescope.nvim",
     },
   },
+
+  {
+    "HakonHarnes/img-clip.nvim",
+    cmd = { "PasteImage", "ImgClipDebug" },
+    opts = {
+      default = {
+        embed_image_as_base64 = false,
+        prompt_for_file_name = false,
+        drag_and_drop = {
+          insert_mode = true,
+        },
+      },
+    },
+    keys = {
+      { "<leader>pi", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+    },
+  },
 }
